26b39095cdc2b3b001c5bf4d640ad3ee916aedd0
Joost Meijles <joost.meijles@avivasolutions.nl>
2025-08-27 11:55:31 +0200

    Use secret directly in client component

---
+++ app/inside-component/page.tsx
@@ -0,0 +1,6 @@
+import { ClientComponent } from "./ClientComponent";
+
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return <ClientComponent />;
+}
