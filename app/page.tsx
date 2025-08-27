bb3de053d5fdf697205e5f5137d0bf5ee4c46f99
Joost Meijles <joost.meijles@avivasolutions.nl>
2025-08-27 10:42:43 +0200

    Do not copy

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { ClientComponent } from "./ClientComponent";
 
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  const props = { key : process.env.SECRET };
-  
-  return <ClientComponent {...props}/>;
+  //const props = { key : process.env.SECRET };
+
+  return <ClientComponent key={process.env.SECRET}/>;
 }
